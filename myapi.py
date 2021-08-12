45bce8b9bf881c65baec5b651eeef641096aa86e
madhav06 <mnandan06@gmail.com>
2021-08-13 00:00:34 +0530

    Adding endpoints

--- myapi.py
+++ myapi.py
@@ -4,6 +4,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "john",
+        "age": 17,
+        "class": "year 2019"
+    }
+}
+
 '''
 GET - GET an Information
 POST - Create Something new
@@ -13,3 +21,7 @@ DELETE - Delete something
 @app.get("/")
 def index():
     return { "name": "First Data" } 
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
