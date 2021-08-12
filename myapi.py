412c2c848bd5d2db29d190797624647959a5d1a7
madhav06 <mnandan06@gmail.com>
2021-08-12 23:45:40 +0530

    Updating FastAPI app

--- myapi.py
+++ myapi.py
@@ -4,4 +4,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+'''
+GET - GET an Information
+POST - Create Something new
+PUT - Update
+DELETE - Delete something
+'''
+@app.get("/")
+def index():
+    return { "name": "First Data" } 
