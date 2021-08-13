5adb389ae63440597daa6608208ef6e993700a19
madhav06 <mnandan06@gmail.com>
2021-08-13 12:19:45 +0530

    POST method

--- myapi.py
+++ myapi.py
@@ -1,6 +1,6 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,7 +8,7 @@ students = {
     1: {
         "name": "john",
         "age": 17,
-        "class": "year 2019"
+        "year": "year 2019"
     }
 }
 
@@ -18,10 +18,33 @@ POST - Create Something new
 PUT - Update
 DELETE - Delete something
 '''
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+
+
 @app.get("/")
 def index():
     return { "name": "First Data" } 
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int):
+def get_student(student_id: int = Path(None, description="The ID of the student you want here.", gt=0)):
+    return students[student_id]
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id:int, name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student Exists"}
+
+    students[student_id] = student
     return students[student_id]
